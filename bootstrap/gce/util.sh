61ca16af3b4fc3d2c119fb1d8f5157c4f806b794
enxebre <alberto.garcial@hotmail.com>
2015-06-03 13:54:24 +0100

    GCE support.

--- bootstrap/gce/util.sh
+++ bootstrap/gce/util.sh
@@ -44,7 +44,6 @@ ansible_ssh_config() {
     ControlPath            ~/.ssh/mux-%r@%h:%p
     ControlPersist         30m
     User                   ubuntu
-    IdentityFile           $TF_VAR_key_file
     UserKnownHostsFile     /dev/null
 EOF
   popd
@@ -52,9 +51,8 @@ EOF
 
 ansible_playbook_run() {
   pushd $APOLLO_ROOT
-    AWS_ACCESS_KEY_ID=${TF_VAR_access_key} AWS_SECRET_ACCESS_KEY=${TF_VAR_secret_key} \
-    ANSIBLE_SSH_ARGS="-F $APOLLO_ROOT/terraform/${APOLLO_PROVIDER}/ssh.config -q" \
-    ansible-playbook --user=ubuntu --inventory-file=$APOLLO_ROOT/inventory \
+    ANSIBLE_SSH_ARGS="-F $APOLLO_ROOT/terraform/${APOLLO_PROVIDER}/ssh.config -q"
+    ansible-playbook --inventory-file=$APOLLO_ROOT/inventory \
     --extra-vars "consul_atlas_infrastructure=${ATLAS_INFRASTRUCTURE} \
       consul_atlas_join=true \
       consul_atlas_token=${ATLAS_TOKEN} \
@@ -65,9 +63,7 @@ ansible_playbook_run() {
 
 apollo_down() {
   pushd $APOLLO_ROOT/terraform/${APOLLO_PROVIDER}
-    terraform destroy -var "access_key=${TF_VAR_access_key}" \
-      -var "key_file=${TF_VAR_key_file}" \
-      -var "region=${TF_VAR_region}"
+    terraform destroy -var "region=${TF_VAR_region}"
   popd
 }
 
@@ -77,15 +73,5 @@ terraform_apply() {
       -var "instance_type.slave=${TF_VAR_slave_size}" \
       -var "atlas_artifact.master=${TF_VAR_atlas_artifact_master}" \
       -var "atlas_artifact.slave=${TF_VAR_atlas_artifact_slave}"
-  popd
-}
-
-open_urls() {
-  pushd $APOLLO_ROOT/terraform/${APOLLO_PROVIDER}
-    if [ -a /usr/bin/open ]; then
-      /usr/bin/open "http://$(terraform output master.1.ip):5050"
-      /usr/bin/open "http://$(terraform output master.1.ip):8080"
-      /usr/bin/open "http://$(terraform output master.1.ip):8500"
-    fi
   popd
 }
